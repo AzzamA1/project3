265389ab0b72e13fd7d9e52c08b94dadbd6013ba
AzzamHamdan <112507056+AzzamA1@users.noreply.github.com>
2022-11-22 05:21:10 -0600

    Added center position for map

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 function initMap(){
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
-  }
+	center: { lat: 41.88233, lng: -87.62327},
+  });
+  
 }
 
 window.initMap = initMap;
