0085b19b22bf1cbf4210847904caeb50ac3beca5
AzzamHamdan <112507056+AzzamA1@users.noreply.github.com>
2022-11-22 20:21:23 -0600

    Updated marker, content, and center

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 function initMap(){
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
-	center: { lat: 41.88233, lng: -87.62327},
+	center: { lat: 41.8781, lng: 87.6298},
   });
   
   var marker = new google.maps.Marker({
-    position: { lat: 41.88233, lng: -87.62327},
+    position: { lat: 41.8826, lng: -87.6233},
 	map: map,
   });
   
   var infowindow = new google.maps.InfoWindow({
-    content: "This is info"
+    content: "This is the Cloud Gate"
   });
   
   infowindow.open(map, marker);
